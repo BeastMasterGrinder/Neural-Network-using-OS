b3395a1ebafc95c2a836ce790180a5c61b4c4926
arfatkh <arfatkhan525@gmail.com>
2023-05-10 19:10:16 +0500

    updates

--- configReader.h
+++ configReader.h
@@ -6,20 +6,19 @@
 
 struct NeuralNetworkConfig {
     std::vector<std::vector<double>> inputLayerWeights;
-    std::vector<std::vector<double>> hiddenLayerWeights;
+    std::vector<std::vector<std::vector<double>>> hiddenLayerWeights;
     std::vector<std::vector<double>> outputLayerWeights;
     std::vector<double> inputData;
     
-    //constructor
-    NeuralNetworkConfig(const std::string& filename){
-        read(filename);
     
+    // Constructor
+    NeuralNetworkConfig(const std::string& filename) {
+        read(filename);
     }
+    
     void read(const std::string& filename);
     void print();
-    int NHiddenLayers(){
-        return hiddenLayerWeights.size();
-    }
+    int NHiddenLayers();
 };
 
 void NeuralNetworkConfig::read(const std::string& filename) {
@@ -29,22 +28,21 @@ void NeuralNetworkConfig::read(const std::string& filename) {
     enum class Section { IL, HL, OL, ID, None };
     Section currentSection = Section::None;
     int hiddenLayerCount = 0;
-    bool isInputLayer = true;
 
     while (std::getline(file, line)) {
         if (line.empty())
             continue;
 
-        if (line == "IL") {
+        if (line == "IL")
             currentSection = Section::IL;
-            isInputLayer = true;
-        } else if (line == "OL")
+        else if (line == "OL")
             currentSection = Section::OL;
         else if (line == "ID")
             currentSection = Section::ID;
         else if (line.substr(0, 2) == "HL") {
             currentSection = Section::HL;
             hiddenLayerCount++;
+            hiddenLayerWeights.emplace_back();  // Add a new hidden layer
             continue;
         }
 
@@ -61,11 +59,6 @@ void NeuralNetworkConfig::read(const std::string& filename) {
 
             inputData = values;
         } else {
-            if (isInputLayer) {
-                isInputLayer = false;
-                continue;
-            }
-
             while (iss >> value) {
                 values.push_back(value);
                 if (iss.peek() == ',')
@@ -77,7 +70,7 @@ void NeuralNetworkConfig::read(const std::string& filename) {
                     inputLayerWeights.push_back(values);
                     break;
                 case Section::HL:
-                    hiddenLayerWeights.push_back(values);
+                    hiddenLayerWeights[hiddenLayerCount - 1].push_back(values);
                     break;
                 case Section::OL:
                     outputLayerWeights.push_back(values);
@@ -88,74 +81,11 @@ void NeuralNetworkConfig::read(const std::string& filename) {
         }
     }
 
-    // Adjust hidden layer weights to match the actual number of hidden layers
-    hiddenLayerWeights.resize(hiddenLayerCount);
+
+    //remove the first empy vector in inputLayerWeights
+    inputLayerWeights.erase(inputLayerWeights.begin());
+
 }
-
-
-// void NeuralNetworkConfig::read(const std::string& filename) {
-//     std::ifstream file(filename);
-//     std::string line;
-
-//     enum class Section { IL, HL, OL, ID, None };
-//     Section currentSection = Section::None;
-//     int hiddenLayerCount = 0;
-
-//     while (std::getline(file, line)) {
-//         if (line.empty())
-//             continue;
-
-//         if (line == "IL")
-//             currentSection = Section::IL;
-//         else if (line == "OL")
-//             currentSection = Section::OL;
-//         else if (line == "ID")
-//             currentSection = Section::ID;
-//         else if (line.substr(0, 2) == "HL") {
-//             currentSection = Section::HL;
-//             hiddenLayerCount++;
-//             continue;
-//         }
-
-//         std::istringstream iss(line);
-//         std::vector<double> values;
-//         double value;
-
-//         if (currentSection == Section::ID) {
-//             while (iss >> value) {
-//                 values.push_back(value);
-//                 if (iss.peek() == ',')
-//                     iss.ignore();
-//             }
-
-//             inputData = values;
-//         } else {
-//             while (iss >> value) {
-//                 values.push_back(value);
-//                 if (iss.peek() == ',')
-//                     iss.ignore();
-//             }
-
-//             switch (currentSection) {
-//                 case Section::IL:
-//                     inputLayerWeights.push_back(values);
-//                     break;
-//                 case Section::HL:
-//                     hiddenLayerWeights.push_back(values);
-//                     break;
-//                 case Section::OL:
-//                     outputLayerWeights.push_back(values);
-//                     break;
-//                 default:
-//                     break;
-//             }
-//         }
-//     }
-
-//     // Adjust hidden layer weights to match the actual number of hidden layers
-//     hiddenLayerWeights.resize(hiddenLayerCount);
-
-// }
 
 void NeuralNetworkConfig::print() {
     std::cout << "Input Layer Weights:\n";
@@ -168,8 +98,11 @@ void NeuralNetworkConfig::print() {
 
     std::cout << "\nHidden Layer Weights:\n";
     for (const auto& layer : hiddenLayerWeights) {
-        for (const auto& weight : layer) {
-            std::cout << weight << " ";
+        for (const auto& neuron : layer) {
+            for (const auto& weight : neuron) {
+                std::cout << weight << " ";
+            }
+            std::cout << "\n";
         }
         std::cout << "\n";
     }
@@ -189,8 +122,9 @@ void NeuralNetworkConfig::print() {
     std::cout << "\n";
 }
 
-// int main() {
-//     NeuralNetworkConfig config = readNeuralNetworkConfig("config.txt");
-//     printNeuralNetworkConfig(config);
-//     return 0;
-// }
+int NeuralNetworkConfig::NHiddenLayers() {
+    return hiddenLayerWeights.size();
+}
+
+
+   
